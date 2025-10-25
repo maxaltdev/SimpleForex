6b2cd7a2bcaf091c4e1538fa0bebdb1a9b1d30aa
Max <foss@maxalt.dev>
2025-10-25 16:18:08 +0300

    Add ForexDataService

---
+++ src/main/java/dev/maxalt/simpleforex/ForexDataService.java
@@ -0,0 +1,62 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2025, Maxim Altoukhov
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package dev.maxalt.simpleforex;
+
+import org.jspecify.annotations.NullMarked;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Currency;
+import java.util.Set;
+
+/// An abstract service that provides currency and exchange rate data.
+@NullMarked
+public interface ForexDataService {
+
+    /// Fetches exchange rates on a specific date.
+    ///
+    /// @param base   the base currency, not `null`
+    /// @param quotes the quote currencies to target, no `null`s (neither the collection nor its contents)
+    /// @param date   an arbitrary date, not `null`
+    /// @return a set of exchange rates that match the given query, or an empty set if none do
+    Set<ExchangeRate> getExchangeRates(Currency base, Collection<Currency> quotes, LocalDate date);
+
+    /// Fetches exchange rates within the specified date range.
+    ///
+    /// The order of the dates doesn't matter.
+    /// If both dates are the same then this is equivalent to calling [#getExchangeRates(Currency, Collection, LocalDate)].
+    ///
+    /// @param base   the base currency, not `null`
+    /// @param quotes the quote currencies to target, no `null`s (neither the collection nor its contents)
+    /// @param start  an arbitrary date, not `null`
+    /// @param end    an arbitrary date, not `null`
+    /// @return a set of exchange rates that match the given query, or an empty set if none do
+    Set<ExchangeRate> getExchangeRates(Currency base, Collection<Currency> quotes, LocalDate start, LocalDate end);
+}
